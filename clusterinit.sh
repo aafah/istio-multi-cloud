0d757deab6560b75da8b1559fec00f1262bbd334
root <root@BLU21794.replynet.prv>
2023-04-03 18:16:57 +0200

    postgres-keycloak

--- clusterinit.sh
+++ clusterinit.sh
@@ -1,4 +1,5 @@
 istioctl install -y --set profile=demo --set values.global.proxy.privileged=true
 kubectl create namespace appspace
+kubectl create namespace kcloak
 kubectl label namespace appspace istio-injection=enabled
 ./dockerbuildall.sh
