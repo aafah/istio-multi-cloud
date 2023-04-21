daba2aeaf3668f65ed67d046bd3d530915e510be
amart <admartiradonna@gmail.com>
2023-04-21 11:26:48 +0200

    clusterbuild-fix

--- clusterbuild.sh
+++ clusterbuild.sh
@@ -8,4 +8,12 @@ kubectl apply -f istioconf.yaml
 kubectl apply -f policy.yaml
 kubectl apply -f postgres.yaml
 kubectl apply -f keycloak.yaml
+kubectl apply -f redis.yaml
 kubectl apply -f oauth2.yaml
+
+#kubectl delete -f keycloak.yaml
+#psql -h 127.0.0.1 -p 42223 -U p-user postgres
+#DELETE DATABASE keycloak;
+#CREATE DATABASE keycloak;
+#psql -h 127.0.0.1 -p 42223 -d keycloak -U p-user < tesi/first/sqlcloak.db
+#
