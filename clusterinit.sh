af0d30dbbf1ced330eb097d5b3e380decfd50116
Andrea Martiradonna <admar@ip-172-31-28-220.eu-west-3.compute.internal>
2023-05-18 12:20:31 +0000

    multiprimary

--- clusterinit.sh
+++ clusterinit.sh
@@ -9,19 +9,22 @@ MY_SERVICE_IP=$1
 
 SECONDS=0
 echo "[1/6] Starting minikube..."
-minikube start --mount-string=/home/admar/first:/host --mount --cpus 4 --memory 10240
+minikube start --mount-string=/home/admar/first:/host --mount \
+--cpus 4 --memory 10240 \
+--apiserver-ips $MY_SERVICE_IP
 
 echo "Setting up certificates..."
 kubectl create namespace istio-system
 cd cert
-make -f ../istio-1.17.2/tools/certs/Makefile.selfsigned.mk root-ca
-make -f ../istio-1.17.2/tools/certs/Makefile.selfsigned.mk cluster1-cacerts
+rm -r *
+make -f ../../istio-1.17.2/tools/certs/Makefile.selfsigned.mk root-ca
+make -f ../../istio-1.17.2/tools/certs/Makefile.selfsigned.mk cluster1-cacerts
 cd ..
 kubectl create secret generic cacerts -n istio-system \
-      --from-file=cluster1/ca-cert.pem \
-      --from-file=cluster1/ca-key.pem \
-      --from-file=cluster1/root-cert.pem \
-      --from-file=cluster1/cert-chain.pem
+      --from-file=cert/cluster1/ca-cert.pem \
+      --from-file=cert/cluster1/ca-key.pem \
+      --from-file=cert/cluster1/root-cert.pem \
+      --from-file=cert/cluster1/cert-chain.pem
 
 echo "[2/6] Configuring the mesh..."
 
@@ -47,7 +50,8 @@ kubectl apply -f metal.yaml
 
 echo "Cluster now up and running, have fun!"
 ./multicluster.sh
-echo "CA_BUNDLE=$(kubectl get secret cacerts -n istio-system -o jsonpath={.data."ca-cert\.pem"})"
+
+./remote.sh $MY_SERVICE_IP
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo $ELAPSED
 echo " "
