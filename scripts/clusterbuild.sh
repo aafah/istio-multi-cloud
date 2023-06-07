abb2a56149537096a8f1c97353e143518946d4cd
Andrea Martiradonna <admar@ip-172-31-28-220.eu-west-3.compute.internal>
2023-06-07 12:12:38 +0000

    fixed paths

--- scripts/clusterbuild.sh
+++ scripts/clusterbuild.sh
@@ -7,7 +7,7 @@ kubectl apply -f res/istio/policy.yaml --context='cube1'
 
 kubectl apply -f res/deplo/microtwo.yaml --context='cube1'
 kubectl apply -f res/deplo/microdb.yaml --context='cube1'
-kubectl apply -f res/deplo/microusr.yaml --context='cube2'
+kubectl apply -f res/deplo/microusr.yaml --context='cube1'
 kubectl apply -f res/deplo/react.yaml --context='cube1'
 kubectl apply -f res/deplo/probe.yaml --context='cube1'
 kubectl apply -f res/istio/igate.yaml --context='cube1'
