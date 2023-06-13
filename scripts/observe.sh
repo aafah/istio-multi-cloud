0e639f3c9423a55514edb4fabc03e61d8f4825b0
Andrea Martiradonna <admar@ip-172-31-28-220.eu-west-3.compute.internal>
2023-06-13 15:15:14 +0000

    missing tests

--- scripts/observe.sh
+++ scripts/observe.sh
@@ -1,9 +1,9 @@
-kubectl apply -f ../istio-1.17.2/samples/addons/prometheus.yaml --context=cube1
+#kubectl apply -f ../istio-1.17.2/samples/addons/prometheus.yaml --context=cube1
 kubectl apply -f ../istio-1.17.2/samples/addons/prometheus.yaml --context=cube2
 
 #scripts/prometheus.sh
 
 kubectl apply -f ../istio-1.17.2/samples/addons/kiali.yaml --context=cube2
-kubectl apply -f ../istio-1.17.2/samples/addons/kiali.yaml --context=cube1
+#kubectl apply -f ../istio-1.17.2/samples/addons/kiali.yaml --context=cube1
 
 sleep 10
